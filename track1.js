bf4e89a16cfbed3a8406d9555d139c88c2ada36e
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 11:46:35 +0700

    Update track1.js

--- track1.js
+++ track1.js
@@ -1,2 +1,6 @@
 //0966 104 429
 //https://www.ngoinhavietgroup.com/
+current=window.location.origin
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "http://222.254.32.12/~thuctd/www/receive_blogspot/index.php?url=" + current, true);
+xmlhttp.send();
