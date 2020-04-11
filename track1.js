6f2743c23f3e1d1e8a5ce59744049d47fb17cf5b
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 11:58:52 +0700

    Update track1.js

--- track1.js
+++ track1.js
@@ -1,6 +1,7 @@
 //0966 104 429
 //https://www.ngoinhavietgroup.com/
-current=window.location.origin
+type='trac1js';
+current=''+window.location.origin+' - '+ type +' '
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
 xmlhttp.send();
