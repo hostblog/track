7e8ab6e4c66d5fff963f45f3758d4f700e4f6f0d
hostblog <39641129+hostblog@users.noreply.github.com>
2020-04-11 11:52:55 +0700

    Update track1.js

--- track1.js
+++ track1.js
@@ -2,5 +2,5 @@
 //https://www.ngoinhavietgroup.com/
 current=window.location.origin
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "http://222.254.32.12/~thuctd/www/receive_blogspot/index.php?url=" + current, true);
+xmlhttp.open("GET", "https://vanphongphamnhatminh.com/namcuong/receive_blogspot/index.php?url=" + current, true);
 xmlhttp.send();
